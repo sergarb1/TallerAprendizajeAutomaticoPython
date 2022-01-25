25229a54a50b9f80df18a7475622ee2743fd2b3b
sergarb1 <sergi.profesor@gmail.com>
2022-01-26 00:26:12 +0100

    Ejemplos vistosos

--- EjemplosPracticos/EJ01-DeteccionCoches/EjemploCoches.py
+++ EjemplosPracticos/EJ01-DeteccionCoches/EjemploCoches.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 
-#pip3 install opencv-python pillow tensorflow cvlib
+#pip3 install --upgrade opencv-python pillow tensorflow cvlib numpy
+#OJO Error con version moderna de opencv-python https://stackoverflow.com/questions/69719025/cvlib-causing-indexerror-invalid-index-to-scalar-variable
+#Se arregla con pip3 install opencv-python==4.5.3.56)
 
 import cv2
 import matplotlib.pyplot as plt
@@ -9,7 +11,7 @@ import cvlib as cv
 from cvlib.object_detection import draw_bbox
 
 
-im = cv2.imread('alumnos.jpg')
+im = cv2.imread('cars_4.jpg')
 
 bbox, label, conf = cv.detect_common_objects(im)
 output_image = draw_bbox(im, bbox, label, conf)
--- EjemplosPracticos/EJ02-ReconocimientoVideoCara/EjemploReconocimientoVideoCara.py
+++ EjemplosPracticos/EJ02-ReconocimientoVideoCara/EjemploReconocimientoVideoCara.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-#pip3 install opencv-python pillow tensorflow cvlib
+
+#pip3 install --upgrade opencv-python pillow tensorflow cvlib numpy
+#OJO Error con version moderna de opencv-python https://stackoverflow.com/questions/69719025/cvlib-causing-indexerror-invalid-index-to-scalar-variable
+#Se arregla con pip3 install opencv-python==4.5.3.56)
+
 import cv2
 
 # Enable camera
